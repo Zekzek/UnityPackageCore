a9eab0b817c34c636ae3daf75d0614ef4ec905a2
Al Bueche <albert.bueche.iv@gmail.com>
2022-09-02 23:37:05 -0700

    scaled noise and gradually changing bush models

--- Assets/Features/HexWorld/View/BushObjectBehaviour.cs
+++ Assets/Features/HexWorld/View/BushObjectBehaviour.cs
@@ -11,31 +11,31 @@ namespace Zekzek.HexWorld
 
         private void SetMesh(int seed)
         {
-            SetMesh(
-                GenerationUtil.GetColor(Model.Location.GridIndex),
-                2 + Noise.Noise.GetPositiveInt(seed) % 2, 
-                2 + Noise.Noise.GetPositiveInt(seed, 1) % 4,
-                90 + Noise.Noise.GetPositiveInt(seed, 1) % 180,
-                new Vector2(0.5f + Noise.Noise.GetPercent(seed, 2) / 2f, 0.1f + Noise.Noise.GetPercent(seed, 3) / 4f),
-                new Vector2(0.1f + Noise.Noise.GetPercent(seed, 4) / 3f, Noise.Noise.GetPercent(seed, 3) / 4f));
+            Vector2Int index = Model.Location.GridIndex;
+            int pointCount = 4;// (int)(2 + 4 * Noise.Noise.SteppedPercent(19, index.x, index.y, 1));
+            SetMesh(GenerationUtil.GetColor(Model.Location.GridIndex),
+                layerCount: 3, pointCount: pointCount,
+                spin: (int)(180f / pointCount * (0.5f + Noise.Noise.SteppedPercent(9, index.x, index.y, 2))),
+                peak: new Vector2(0.5f + 0.5f * Noise.Noise.SteppedPercent(13, index.x, index.y, 3), 0.1f + 0.2f * Noise.Noise.SteppedPercent(13, index.x, index.y, 4)),
+                valley: new Vector2(0.1f + 0.5f * Noise.Noise.SteppedPercent(23, index.x, index.y, 5), 0.2f * Noise.Noise.SteppedPercent(23, index.x, index.y, 6)));
         }
 
-        private void SetMesh(Vector3 color, int layerCount, int pointCount, int spinStep, Vector2 peak, Vector2 valley)
+        private void SetMesh(Vector3 color, int layerCount, int pointCount, int spin, Vector2 peak, Vector2 valley)
         {
             foreach(GameObject child in _children) {
                 child.SetActive(false);
-            }
+            } 
 
             Mesh disk = null;
             Mesh outline = null;
             Vector3 scale = Vector3.one;
-            float spin = 0;
+            float angle = 0;
             for(int i = 0; i < layerCount; i++) {
                 Vector3[] points = GetPointsFromSpokes(pointCount, peak, valley);
 
                 while (2 * i >= _children.Count) {
                     GameObject diskObect = new GameObject("Disk" + i);
-                    diskObect.transform.parent = gameObject.transform;
+                    diskObect.transform.parent = gameObject.transform;  
                     diskObect.transform.localPosition = Vector3.zero;
                     _children.Add(diskObect);
                     GameObject outlineObect = new GameObject("Outline" + i);
@@ -47,17 +47,17 @@ namespace Zekzek.HexWorld
                 disk ??= GetDiskMesh(points);
                 ApplyMesh(_children[2 * i], Vector3.zero, disk);
                 _children[2 * i].transform.localScale = scale;
-                _children[2 * i].transform.localRotation = Quaternion.Euler(0, spin, 0);
+                _children[2 * i].transform.localRotation = Quaternion.Euler(0, angle, 0);
                 _children[2 * i].SetActive(true);
 
                 outline ??= GetOutlineMesh(0.05f, points);
                 ApplyMesh(_children[2 * i + 1], color, outline);
                 _children[2 * i + 1].transform.localScale = scale;
-                _children[2 * i + 1].transform.localRotation = Quaternion.Euler(0, spin, 0);
+                _children[2 * i + 1].transform.localRotation = Quaternion.Euler(0, angle, 0);
                 _children[2 * i + 1].SetActive(true);
 
-                scale = new Vector3(0.7f * scale.x, 1.3f * scale.y, 0.7f * scale.z);
-                spin += spinStep;
+                scale = new Vector3(0.8f * scale.x, 1.5f * scale.y, 0.8f * scale.z);
+                angle += spin;
             }
         }
     }
--- Assets/Features/Noise/Noise.cs
+++ Assets/Features/Noise/Noise.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Zekzek.Noise
 {
     public static class Noise
@@ -29,6 +31,27 @@ namespace Zekzek.Noise
         {
             int value = (int)GetUint(key1, key2, key3, seed, offset);
             return value < 0 ? -value : value;
+        }
+
+        public static float SteppedPercent(int stepSize, int rawX, int rawZ, uint offset)
+        {
+            float x = rawX / (float)stepSize;
+            float z = rawZ / (float)stepSize;
+            int floorX = (int)Math.Floor(x);
+            int floorZ = (int)Math.Floor(z);
+            float xRatio = x - floorX;
+            float zRatio = z - floorZ;
+
+            float floorXFloorZ = GetPercent(floorX, floorZ, seed: offset);
+            float floorXCeilZ = GetPercent(floorX, floorZ + 1, seed: offset);
+            float ceilXFloorZ = GetPercent(floorX + 1, floorZ, seed: offset);
+            float ceilXCeilZ = GetPercent(floorX + 1, floorZ + 1, seed: offset);
+
+            return
+                ((1 - xRatio) * (1 - zRatio)) * floorXFloorZ +
+                ((1 - xRatio) * zRatio) * floorXCeilZ +
+                (xRatio * (1 - zRatio)) * ceilXFloorZ +
+                (xRatio * zRatio) * ceilXCeilZ;
         }
 
         public static float GetPercent(int key1, int key2 = 0, int key3 = 0, uint seed = 0, uint offset = 0)
